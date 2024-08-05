159be038a519d6ebd4882492e39b172a652f2be0
Lucas Lois <lucasloisp@gmail.com>
2024-08-05 19:03:00 -0300

    work fish sync

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,25 +29,21 @@ set -x LANG en_US.UTF-8
 set snorin_chevrons blue white blue white
 set snorin_show_git_prefix
 
-# Ruby Development Setup
-switch (uname)
-case Darwin
-    status --is-interactive; and . (rbenv init -|psub)
-end
-
 # Android Development Setup
 switch (uname)
 case Darwin
     abbr diskusage 'ncdu'
 
-    set -x JAVA_HOME "$HOME/.sdkman/candidates/java/as-embedded"
+    set -x JAVA_HOME "/Applications/Android Studio.app/Contents/jbr/Contents/Home/"
     set -x ANDROID "$HOME/Library/Android"
     set -x ANDROID_HOME "$ANDROID/sdk"
+    set -x ANDROID_SDK_ROOT "$ANDROID/sdk"
     set -x nvm_default_version v16.15.1
     fish_add_path $ANDROID_HOME/emulator
     fish_add_path $ANDROID_HOME/tools
     fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin/
     fish_add_path $ANDROID_HOME/platform-tools
+    fish_add_path /usr/local/bin
     fish_add_path /opt/homebrew/bin
     fish_add_path "$HOME/.cargo/bin"
 case Linux
@@ -57,4 +53,10 @@ case Linux
     set -x ANDROID $HOME/Android
     set -x ANDROID_HOME $ANDROID/Sdk
     set -x SYSTEMD_EDITOR vim
+end
+
+# Ruby Development Setup
+switch (uname)
+case Darwin
+    status --is-interactive; and . (rbenv init -|psub)
 end
