9b6aecc4a1cb82f1fca61219a778a2684ddab664
Lucas Lois <lucasloisp@gmail.com>
2026-03-27 10:53:48 -0400

    fish: set up pnpm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -70,3 +70,10 @@ end
 
 # NodeJS development setup
 bass source ~/.nvm/nvm.sh
+
+# pnpm
+set -gx PNPM_HOME "/Users/llois-c/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
